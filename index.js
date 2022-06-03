23041b4866c2e42f9979253b58a1e70192c307ac
Aryan Babber <86878236+AryanBabber@users.noreply.github.com>
2022-06-04 01:34:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const header = document.getElementById('header')
+const maincontent = document.getElementById('main')
+const footer = document.getElementById('footer')
+
+const windowWidth = window.innerWidth
+
+console.log(windowWidth)
+
+if(windowWidth < 1920){
+    console.log('Too small')
+}
